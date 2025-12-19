9d050aab49cf12723f8448c0e2118a7eabb9f29c
enkryptonloll <136836328+enkryptonloll@users.noreply.github.com>
2025-12-19 17:48:43 +0000

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,8 @@
 pluginManagement {
     repositories {
-        maven {
-            name = "Fabric"
-            url = uri("https://maven.fabricmc.net/")
-        }
-        mavenCentral()
         gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        mavenCentral()
     }
 }
 
